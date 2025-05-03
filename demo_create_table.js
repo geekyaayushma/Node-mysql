c129d8498f40f603e12d9b16fca0b27c731d1851
geekyaayushma <mahataayushma653@gmail.com>
2025-05-03 22:02:40 +0545

    Updated connection in while creating table

--- demo_create_table.js
+++ demo_create_table.js
@@ -1,19 +1,20 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
-const connection = mysql.createconnection(
-    host = process.env.DB_HOST;
-    user = process.env.DB_USER;
-    password = process.env.DB_PASSWORD;
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "mydb",
+});
 
-);
-
-
-connection.connect(funrtion(err){
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  const sql =
+    "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  connection.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Connected!");
-    var sql 
-    connection.query("INSERT")
-    if (err) throw err;
-       
-})
+    console.log("Table created");
+  });
+});
