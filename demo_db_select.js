e78307f7fbd78639114d5d0f6bb32677ffdd4b59
geekyaayushma <mahataayushma653@gmail.com>
2025-05-06 22:43:26 +0545

    Added how to select in database

---
+++ demo_db_select.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "mydb",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "SELECT * FROM customers";
+  connection.query(sql, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    console.log(fields);
+  });
+});
