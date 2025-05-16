3fe35a55216ca9c9f194766c0f2fcc7a08dd5056
geekyaayushma <mahataayushma653@gmail.com>
2025-05-16 22:35:13 +0545

    Added orderby

--- demo_db_orderby.js
+++ demo_db_orderby.js
@@ -1,19 +1,19 @@
-require.('dotenv').config();
-const mysql = require('mysql');
+require("dotenv").config();
+const mysql = require("mysql");
 
 const connection = mysql.createConnection.connection({
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASSWORD,
-    database : 'mydb'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "mydb",
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected");
+  const sql = "SELECT * FROM CUSTOMERS ORDER BY name";
+  connection.query(sql, function (err, result) {
     if (err) throw err;
-    console.log('Connected');
-    const sql = "SELECT * FROM CUSTOMERS ORDER BY name";
-    connection.query(sql, function(err, result){
-        if (err) throw err;
-        console.log(result);
-    });
+    console.log(result);
+  });
 });
