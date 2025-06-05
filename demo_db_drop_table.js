d452d491b1cfafff6c3a23bff5de7b1d48795e9e
geekyaayushma <mahataayushma653@gmail.com>
2025-06-05 08:56:55 +0545

    Dropped table customers

---
+++ demo_db_drop_table.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "mydb",
+});
+
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected");
+  var sql = "DROP TABLE customers";
+  //Delete the whole table
+  connection.query(sql, function (err) {
+    if (err) throw err;
+    console.log("Table deleted");
+  });
+});
